30acc3c271cb9976b51b33811c3860972596dc82
Oleksandr Melnyk <melnykoleksandr724@gmail.com>
2026-03-20 21:41:22 +0100

    pause

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,2 +1,39 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(image => {
+      return `<li class="gallery-item">
+            <a href="${image.largeImageURL}">
+              <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/>
+            </a>
+            <p><b>likes</b>: ${image.likes}</p>
+            <p><b>views</b>: ${image.views}</p>
+            <p><b>comments</b>: ${image.comments}</p>
+            <p><b>downloads</b>: ${image.downloads}</p>
+          </li>`;
+    })
+    .join('');
+  refs.gallery.innerHTML = markup;
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  refs.gallery.innerHTML = '';
+}
+export function showLoader() {
+  refs.loader.classList.add('visible');
+}
+export function hideLoader() {
+  refs.loader.classList.remove('visible');
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import { clearGallery, showLoader } from './js/render-functions';
+import { getImagesByQuery } from './js/pixabay-api';
+import { createGallery, hideLoader } from './js/render-functions';
 
 const refs = {
   form: document.querySelector('.form'),
 };
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  const query = e.target.elements['search-text'].value;
+
+  if (query.trim() === '') {
+    iziToast.error({
+      message: 'Please enter a search query',
+    });
+    return;
+  }
+  clearGallery();
+  showLoader();
+  console.log(query);
+  e.target.reset();
+});
