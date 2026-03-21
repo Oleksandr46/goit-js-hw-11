afd3ec5198b256bb6bcf43a7821a43a1551dd93d
Oleksandr Melnyk <melnykoleksandr724@gmail.com>
2026-03-21 13:28:58 +0100

    hgfgh

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import { clearGallery, showLoader } from './js/render-functions';
+import {
+  clearGallery,
+  showLoader,
+  createGallery,
+  hideLoader,
+} from './js/render-functions';
 import { getImagesByQuery } from './js/pixabay-api';
-import { createGallery, hideLoader } from './js/render-functions';
+// import { createGallery, hideLoader } from './js/render-functions';
 
 const refs = {
   form: document.querySelector('.form'),
@@ -10,9 +15,9 @@ const refs = {
 
 refs.form.addEventListener('submit', e => {
   e.preventDefault();
-  const query = e.target.elements['search-text'].value;
+  const query = e.target.elements['search-text'].value.trim();
 
-  if (query.trim() === '') {
+  if (query === '') {
     iziToast.error({
       message: 'Please enter a search query',
     });
@@ -20,6 +25,25 @@ refs.form.addEventListener('submit', e => {
   }
   clearGallery();
   showLoader();
-  console.log(query);
+
+  getImagesByQuery(query)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+        return;
+      }
+      createGallery(data.hits);
+    })
+    .catch(error => {
+      iziToast.error({
+        message: 'Something went wrong. Please try again later.',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
   e.target.reset();
 });
